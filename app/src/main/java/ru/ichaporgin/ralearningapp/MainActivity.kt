429d186cd0596590cd3571ab33d69b7c0c5ad083
Igor Chaporgin <fellclub@yandex.ru>
2025-04-07 21:15:52 +0300

    RA-03 add button and style

--- app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
@@ -3,18 +3,19 @@ package ru.ichaporgin.ralearningapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import ru.ichaporgin.ralearningapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.background_color)
+        window.navigationBarColor = ContextCompat.getColor(this, R.color.background_color)
     }
 }
