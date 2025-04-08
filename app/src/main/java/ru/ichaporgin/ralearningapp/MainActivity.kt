02bf815fce67c49745bec1f91f319e693ebd794d
Igor Chaporgin <fellclub@yandex.ru>
2025-04-08 12:48:52 +0300

    RA-04 add fragment category

--- app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.ichaporgin.ralearningapp
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -9,11 +10,18 @@ import ru.ichaporgin.ralearningapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val fragmentManager = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+        if (findViewById<FrameLayout>(R.id.list_categories) != null) {
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.list_categories, CategoriesListFragment())
+            fragmentTransaction.commit()
+        }
+
         WindowCompat.setDecorFitsSystemWindows(window, true)
         window.statusBarColor = ContextCompat.getColor(this, R.color.background_color)
         window.navigationBarColor = ContextCompat.getColor(this, R.color.background_color)
