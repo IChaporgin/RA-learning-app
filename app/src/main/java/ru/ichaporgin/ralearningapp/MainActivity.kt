87cbf0a90e9ab319b16508bc30222fa0b0f7c020
Igor Chaporgin <fellclub@yandex.ru>
2025-04-08 18:54:22 +0300

    RA-03 refactoring code

--- app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.ichaporgin.ralearningapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
@@ -12,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
         WindowCompat.setDecorFitsSystemWindows(window, true)
         window.statusBarColor = ContextCompat.getColor(this, R.color.background_color)
