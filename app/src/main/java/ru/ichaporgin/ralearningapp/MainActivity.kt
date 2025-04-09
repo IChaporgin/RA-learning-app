933dca6aaa253c98af5c948fb9da717812dddeea
Igor Chaporgin <fellclub@yandex.ru>
2025-04-09 21:23:23 +0300

    RA-05 refactoring code

--- app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/MainActivity.kt
@@ -8,7 +8,6 @@ import ru.ichaporgin.ralearningapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val fragmentManager = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
