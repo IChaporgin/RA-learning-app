64f5545d9399590a62022872265c0dc5284347c6
Igor Chaporgin <fellclub@yandex.ru>
2025-04-16 17:33:07 +0300

    RA-07 refactoring code

--- app/src/main/java/ru/ichaporgin/ralearningapp/CategoriesListFragment.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/CategoriesListFragment.kt
@@ -9,10 +9,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.ichaporgin.ralearningapp.databinding.FragmentListCategoriesBinding
 
-class CategoriesListFragment: Fragment() {
-    private var _binding : FragmentListCategoriesBinding? = null
+class CategoriesListFragment : Fragment() {
+    private var _binding: FragmentListCategoriesBinding? = null
     private val binding
-        get() = _binding ?: throw IllegalStateException("Binding for FragmentListCategoriesBinding must not to be null")
+        get() = _binding
+            ?: throw IllegalStateException("Binding for FragmentListCategoriesBinding must not to be null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
