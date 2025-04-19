301dd9f9c4a61f8def9e0615b2a4a4455098d0b2
Igor Chaporgin <fellclub@yandex.ru>
2025-04-19 16:41:30 +0300

    RA-08 created recyclerview with list item

--- app/src/main/java/ru/ichaporgin/ralearningapp/CategoriesListFragment.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/CategoriesListFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import ru.ichaporgin.ralearningapp.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment() {
