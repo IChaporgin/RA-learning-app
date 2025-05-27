950ef7ac81c0f1fff3d5f26b1b40cb52a3523dba
Igor Chaporgin <fellclub@yandex.ru>
2025-05-27 16:29:35 +0300

    RA-20 refactoring code

--- app/src/main/java/ru/ichaporgin/ralearningapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -2,16 +2,12 @@ package ru.ichaporgin.ralearningapp.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
 import ru.ichaporgin.ralearningapp.data.Constants
-import ru.ichaporgin.ralearningapp.model.Ingredient
+import ru.ichaporgin.ralearningapp.model.Recipe
 
-data class RecipeUiState(
-    var recipeId: Int? = null,
-    var recipeTitle: String? = null,
-    var recipeImage: String? = null,
-    var ingredient: List<Ingredient> = listOf(),
-    var method: List<String> = listOf(),
-    var portion: Int = Constants.MAX_PORTIONS,
-    var isFavorite: Boolean = false,
+data class RecipeState(
+    val recipe: Recipe,
+    val portion: Int = Constants.MAX_PORTIONS,
+    val isFavorite: Boolean = false,
 )
 
 class RecipeViewModel : ViewModel() {
