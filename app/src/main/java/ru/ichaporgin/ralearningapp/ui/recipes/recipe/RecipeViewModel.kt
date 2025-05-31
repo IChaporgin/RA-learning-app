07c5d4812aa75b6a0b5b8a74f27a2d39e88de11e
Igor Chaporgin <fellclub@yandex.ru>
2025-05-31 12:52:01 +0300

    RA-22 subscribe to LiveData

--- app/src/main/java/ru/ichaporgin/ralearningapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,5 +1,7 @@
 package ru.ichaporgin.ralearningapp.ui.recipes.recipe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.ichaporgin.ralearningapp.data.Constants
 import ru.ichaporgin.ralearningapp.model.Recipe
@@ -11,4 +13,10 @@ data class RecipeState(
 )
 
 class RecipeViewModel : ViewModel() {
+    private val mutableSelectedRecipe = MutableLiveData<RecipeState>()
+    val selectedRecipe: LiveData<RecipeState> get() = mutableSelectedRecipe
+
+    init {
+        mutableSelectedRecipe.value = RecipeState(isFavorite = true)
+    }
 }
