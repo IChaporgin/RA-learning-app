b0329e2bde2193f49dbc8c4bd4ef55d93761b6fe
Igor Chaporgin <fellclub@yandex.ru>
2025-07-26 20:37:06 +0300

    RA-40 refactoring code

--- app/src/main/java/ru/ichaporgin/ralearningapp/ui/recipes/favorite/FavoritesViewModel.kt
+++ app/src/main/java/ru/ichaporgin/ralearningapp/ui/recipes/favorite/FavoritesViewModel.kt
@@ -29,11 +29,9 @@ class FavoritesViewModel(application: Application) : AndroidViewModel(applicatio
             val context = getApplication<Application>().applicationContext
             val pref =
                 context.getSharedPreferences(Constants.SHARED_PREFS_NAME, Context.MODE_PRIVATE)
-            val favoriteIds: Set<String?> =
-                pref.getStringSet(Constants.FAVORITES_KEY, emptySet()) ?: emptySet()
-            val idsParam = favoriteIds.filterNotNull().joinToString(",")
+//            val favoriteIds: Set<String?> =
+//                pref.getStringSet(Constants.FAVORITES_KEY, emptySet()) ?: emptySet()
             val recipes = repository.getFavoriteFromCache()
-            //val recipes = repository.getRecipes(idsParam)
             val drawable = try {
                 val inputStream = context.assets.open("bcg_favorites.png")
                 Drawable.createFromStream(inputStream, null)
